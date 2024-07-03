1ffd4bec14a3801df1c256ffe3038bb9062d7c6d
sjamx <Egordudo@mail.ru>
2024-07-03 15:44:32 +0300

    add interval const

--- src/components/carusel/carusel.tsx
+++ src/components/carusel/carusel.tsx
@@ -1,29 +1,43 @@
-import { useState, useEffect, Children, cloneElement } from "react";
+import { useState, useEffect, Children, cloneElement, ReactNode } from "react";
 import styles from "./carusel.module.css";
 
 const oneHundredPercent = "100%";
 
 const childWidth = 450;
 
+const interval = 5000;
+
 export const Carusel = ({ children }) => {
 	const [childrenState, setChildrenState] = useState([]);
 	const [offset, setOffset] = useState(0);
-
+	const [shoodShowBtn, setShoodShowBtn] = useState(false);
 
 	const onClickRightBtn = () => {
 		const maxOffset = -(childWidth * (childrenState.length - 1));
-
-		setOffset(currenOffset => Math.max(currenOffset - childWidth, maxOffset)
-		);
+		setOffset(currenOffset => {
+			if (maxOffset === currenOffset) {
+				return 0;
+			}
+			return Math.max(currenOffset - childWidth, maxOffset);
+		});
 	};
 
 	const onClickLeftBtn = () => {
 		setOffset(currenOffset => Math.min(currenOffset + childWidth, 0));
 	};
 
+	const enterMouseCarusel = () => {
+		setShoodShowBtn(true);
+	};
+
+	const leaveMouseCarusel = () => {
+		setShoodShowBtn(false);
+
+	}
+
 	useEffect(() => {
 		setChildrenState(
-			Children.map(children, (child)=>
+			Children.map(children, child =>
 				cloneElement(child, {
 					style: {
 						height: oneHundredPercent,
@@ -35,11 +49,26 @@ export const Carusel = ({ children }) => {
 		);
 	}, [children]);
 
+	useEffect(() => {
+		let intervalId: number | undefined;
+		if (childrenState.length && !shoodShowBtn) {
+			intervalId = setInterval(onClickRightBtn, interval);
+		} else {
+			clearInterval(intervalId);
+		}
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, [childrenState,shoodShowBtn]);
+
 	return (
-		<div className={styles.container}>
-			<button className={styles.right} onClick={onClickRightBtn}>
-				right
-			</button>
+		<div className={styles.container} onMouseEnter={enterMouseCarusel} onMouseLeave={leaveMouseCarusel}>
+			{shoodShowBtn && (
+				<button className={styles.right} onClick={onClickRightBtn}>
+					right
+				</button>
+			)}
 			<div className={styles.window}>
 				<div
 					className={styles.all_items_container}
@@ -50,9 +79,11 @@ export const Carusel = ({ children }) => {
 					{childrenState}
 				</div>
 			</div>
-			<button className={styles.left} onClick={onClickLeftBtn}>
-				left
-			</button>
+			{shoodShowBtn && (
+				<button className={styles.left} onClick={onClickLeftBtn}>
+					left
+				</button>
+			)}
 		</div>
 	);
 };
